fab59fe3f69b68e35673a51d6423fcee061cf408
AzukiPL <maciejwojtkiewicz101@wp.pl>
2025-02-07 00:27:00 +0100

    slight comment change

--- include/Variables.hpp
+++ include/Variables.hpp
@@ -4,12 +4,13 @@
 namespace Variables {
     // to be implemented: getting values from a file
 
-    inline int width{400}; // Screen Width
-    inline int height{40}; // Screen Height
+    inline int width{400}; // console video Width in character columns
+    inline int height{40}; // console video Height in console rows
 
     inline bool useDefaultVideoFPS{false}; // True = Caps limit to video default fps, takes priority over capFPS
     inline bool capFPS{false}; // True = Caps limit to maxFPS
     inline int maxFPS{50}; // Video FPS limit
+
     inline std::string videoFolder{"../video"}; // folder to scan for video files
-    constexpr inline char gradient[11] = {" ,:;|/?$#@"} ; // gradient of the ascii symbols main ussage in videoConvert::processFrame
+    constexpr inline char gradient[11] = {" ,:;|/?$#@"} ; // gradient of the ascii symbols, main ussage in videoConvert::processFrame
 }     
